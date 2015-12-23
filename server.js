60b0f08f6ac8cf316cacda518d856f8aef41b3fe
Grace Kim <gkim1032@gmail.com>
2015-12-22 20:44:46 -0500

    created basic server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-lkasdjf
+var app = require('express')();
+
+app.get('/', function(req, res) {
+	console.log("REQUEST RECEIVED");
+	res.send("WASSUP BOOBOO");
+});
+
+app.listen(2222);
+console.log("Server is served at port 2222");
